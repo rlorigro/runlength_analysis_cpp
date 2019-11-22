6e707eaceb12fafaff6d2da1a6930205ec8be6c0
ryan <rlorigro@ucsc.edu>
2019-11-22 11:37:45 -0800

    Cleaned up plotting script

--- scripts/plot_confusion_stats.py
+++ scripts/plot_confusion_stats.py
@@ -1,5 +1,6 @@
 from matplotlib import pyplot
 from matplotlib import patches
+from matplotlib.lines import Line2D
 import argparse
 
 
@@ -15,7 +16,6 @@ def read_length_coverage(summary_file_path):
 
             if line[0] == ">":
                 header = line.strip()[1:]
-                print(header)
 
             else:
                 coverage, error = line.strip().split(',')
@@ -74,14 +74,11 @@ def read_percent_error(summary_file_path):
 
             if line[0] == ">":
                 header = line.strip()[1:]
-                print(header)
 
             else:
                 coverage, error = line.strip().split(',')
                 coverage = int(coverage)
                 error = float(error)
-
-                print(coverage, error)
 
                 if header == "base_error":
                     base_error_x.append(coverage)
@@ -95,39 +92,55 @@ def read_percent_error(summary_file_path):
 
 
 def main(input_path):
-    fig, axes = pyplot.subplots(nrows=5)
+    fig, axes = pyplot.subplots(nrows=3)
 
     length_match_coverage_x, length_match_coverage_y, length_mismatch_coverage_x, length_mismatch_coverage_y = read_length_coverage(
         input_path)
 
-    color = (0.122, 0.498, 0.584)
-
-
-    zeros = [0 for i in range(len(length_match_coverage_x))]
-    axes[0].plot(length_match_coverage_x, length_match_coverage_y)
-    axes[0].fill_between(length_match_coverage_x, y1=zeros, y2=length_match_coverage_y, color=color, alpha=0.3)
-    axes[0].set_ylabel("~Total Coverage")
-
-    axes[1].plot(length_mismatch_coverage_x, length_mismatch_coverage_y)
-    axes[1].fill_between(length_match_coverage_x, y1=zeros, y2=length_mismatch_coverage_y, color=color, alpha=0.3)
-    axes[1].set_ylabel("Length Mismatch")
-
     base_match_coverage_x, base_match_coverage_y, base_mismatch_coverage_x, base_mismatch_coverage_y = read_base_coverage(
         input_path)
 
-    axes[2].plot(base_mismatch_coverage_x, base_mismatch_coverage_y)
-    axes[2].fill_between(base_mismatch_coverage_x, y1=zeros, y2=base_mismatch_coverage_y, color=color, alpha=0.3)
-    axes[2].set_ylabel("Base Mismatch")
-
     base_error_x, base_error_y, length_error_x, length_error_y = read_percent_error(input_path)
 
-    axes[3].plot(base_error_x, base_error_y)
-    axes[3].fill_between(base_error_x, y1=zeros, y2=base_error_y, color=color, alpha=0.3)
-    axes[3].set_ylabel("Base Error")
+    length_color = (0.122, 0.498, 0.584)
+    base_color = (0.945, 0.71, 0.176)
 
-    axes[4].plot(length_error_x, length_error_y)
-    axes[4].fill_between(length_error_x, y1=zeros, y2=length_error_y, color=color, alpha=0.3)
-    axes[4].set_ylabel("Length Error")
+    # Find sum of matches + mismatches to get total coverage
+    total_coverage_y = list()
+    for i in range(len(length_match_coverage_y)):
+        total_coverage_y.append(length_match_coverage_y[i] + length_mismatch_coverage_y[i])
+
+    # The lower y bound for "fill" function is just a line along y=0
+    zeros = [0 for i in range(len(length_match_coverage_x))]
+
+    # Make custom legend objects
+    custom_lines = [Line2D([0], [0], color=length_color, lw=4),
+                    Line2D([0], [0], color=base_color, lw=4)]
+
+    axes[2].legend(custom_lines, ["Length", "Base"])
+    axes[1].legend(custom_lines, ["Length", "Base"])
+
+    axes[0].plot(length_match_coverage_x, total_coverage_y)
+    axes[0].fill_between(length_match_coverage_x, y1=zeros, y2=length_match_coverage_y, color=length_color, alpha=0.3)
+
+    axes[0].set_ylabel("Total Coverage")
+
+    axes[1].plot(length_mismatch_coverage_x, length_mismatch_coverage_y)
+    axes[1].fill_between(length_match_coverage_x, y1=zeros, y2=length_mismatch_coverage_y, color=length_color, alpha=0.3)
+
+    axes[1].plot(base_mismatch_coverage_x, base_mismatch_coverage_y)
+    axes[1].fill_between(base_mismatch_coverage_x, y1=zeros, y2=base_mismatch_coverage_y, color=base_color, alpha=0.3)
+
+    axes[1].set_ylabel("Mismatches")
+
+    axes[2].fill_between(length_error_x, y1=zeros, y2=length_error_y, color=length_color, alpha=0.3)
+    axes[2].fill_between(base_error_x, y1=zeros, y2=base_error_y, color=base_color, alpha=0.3)
+    axes[2].plot(length_error_x, length_error_y)
+    axes[2].plot(base_error_x, base_error_y)
+
+    axes[2].set_ylabel("P(Error)")
+
+    axes[2].set_xlabel("Coverage")
 
     pyplot.show()
     pyplot.close()
