64509480c5353085533b85c9d66041a0fb9b782f
ryan <rlorigro@ucsc.edu>
2019-11-25 13:59:41 -0800

    fixed newline bug

--- install_minimap2.sh
+++ install_minimap2.sh
@@ -1,4 +1,3 @@
-curl -L https://github.com/lh3/minimap2/releases/download/v2.17/minimap2-2.17_x64-linux.tar.bz2 | tar -jxvf -
-./minimap2-2.17_x64-linux/minimap2
+curl -L https://github.com/lh3/minimap2/releases/download/v2.17/minimap2-2.17_x64-linux.tar.bz2 | tar -jxvf ./minimap2-2.17_x64-linux/minimap2
 
 export PATH=$(pwd)/minimap2-2.17_x64-linux:$PATH
