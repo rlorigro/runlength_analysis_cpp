fa7c0d088345858318137005427c658b5c916600
ryan <rlorigro@ucsc.edu>
2019-11-25 14:16:05 -0800

    added comment to bashrc append

--- install_minimap2.sh
+++ install_minimap2.sh
@@ -1,3 +1,4 @@
 curl -L https://github.com/lh3/minimap2/releases/download/v2.17/minimap2-2.17_x64-linux.tar.bz2 | tar -jxvf -
+sudo echo "\n#add minimap2 to path\n" >> ~/.bashrc
 sudo echo export PATH=$(pwd)/minimap2-2.17_x64-linux:$PATH >> ~/.bashrc
 source ~/.bashrc
