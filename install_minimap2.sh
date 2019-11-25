a2e4a6334e78bd24ade551281f9928f841d2eefb
ryan <rlorigro@ucsc.edu>
2019-11-25 13:58:20 -0800

    fixed & vs $ bug

--- install_minimap2.sh
+++ install_minimap2.sh
@@ -1,4 +1,4 @@
 curl -L https://github.com/lh3/minimap2/releases/download/v2.17/minimap2-2.17_x64-linux.tar.bz2 | tar -jxvf -
 ./minimap2-2.17_x64-linux/minimap2
 
-export PATH=&(pwd)/minimap2-2.17_x64-linux:$PATH
+export PATH=$(pwd)/minimap2-2.17_x64-linux:$PATH
