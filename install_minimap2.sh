fcb9c082c73e366813d1e7ed764cc3e7a9b7e516
ryan <rlorigro@ucsc.edu>
2019-11-25 14:11:34 -0800

    fixed installation script

--- install_minimap2.sh
+++ install_minimap2.sh
@@ -1,3 +1,3 @@
-curl -L https://github.com/lh3/minimap2/releases/download/v2.17/minimap2-2.17_x64-linux.tar.bz2 | tar -jxvf ./minimap2-2.17_x64-linux/minimap2
-
-export PATH=$(pwd)/minimap2-2.17_x64-linux:$PATH
+curl -L https://github.com/lh3/minimap2/releases/download/v2.17/minimap2-2.17_x64-linux.tar.bz2 | tar -jxvf -
+sudo echo export PATH=$(pwd)/minimap2-2.17_x64-linux:$PATH >> ~/.bashrc
+source ~/.bashrc
